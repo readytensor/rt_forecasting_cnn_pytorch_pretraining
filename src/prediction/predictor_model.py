48af6a9b473ed60cc7600a3f1d0005c102bf9e49
moo3030 <mohamedabdelhamid3030@gmail.com>
2024-02-24 18:09:52 +0200

    updated batch size self.batch_size = max(1, min(train_X.shape[0] // 8, 256))

--- src/prediction/predictor_model.py
+++ src/prediction/predictor_model.py
@@ -20,7 +20,7 @@ import torch.nn.functional as F
 from prediction.pretraining_data_gen import get_pretraining_data
 
 # Check for GPU availability
-device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
 print("device used: ", device)
 
 PREDICTOR_FILE_NAME = "predictor.joblib"
@@ -109,12 +109,25 @@ class Net(Module):
         dim3 = 25
 
         self.conv1 = Conv1d(
-            in_channels=self.feat_dim, out_channels=dim1, kernel_size=4, stride=1, padding='same')
+            in_channels=self.feat_dim,
+            out_channels=dim1,
+            kernel_size=4,
+            stride=1,
+            padding="same",
+        )
         self.conv2 = Conv1d(
-            in_channels=dim1, out_channels=dim2, kernel_size=8, stride=1, padding='same')
+            in_channels=dim1, out_channels=dim2, kernel_size=8, stride=1, padding="same"
+        )
         self.conv3 = Conv1d(
-            in_channels=dim2, out_channels=dim3, kernel_size=16, stride=1, padding='same')
-        self.fc = Linear(in_features=dim3*self.decode_len,  out_features=self.decode_len)
+            in_channels=dim2,
+            out_channels=dim3,
+            kernel_size=16,
+            stride=1,
+            padding="same",
+        )
+        self.fc = Linear(
+            in_features=dim3 * self.decode_len, out_features=self.decode_len
+        )
         self.flatten = Flatten()
 
     def forward(self, X):
@@ -122,8 +135,8 @@ class Net(Module):
         x = self.conv1(x)
         x = self.conv2(x)
         x = self.conv3(x)
-        x = x.permute(0,2,1)
-        x = x[:, -self.decode_len:, :]
+        x = x.permute(0, 2, 1)
+        x = x[:, -self.decode_len :, :]
         x = self.flatten(x)
         x = self.activation(x)
         x = self.fc(x)
@@ -131,11 +144,11 @@ class Net(Module):
         return out
 
     def get_num_parameters(self):
-        pp=0
+        pp = 0
         for p in list(self.parameters()):
-            nn=1
+            nn = 1
             for s in list(p.size()):
-                nn = nn*s
+                nn = nn * s
             pp += nn
         return pp
 
@@ -146,16 +159,12 @@ class Forecaster:
     This class provides a consistent interface that can be used with other
     Forecaster models.
     """
+
     MODEL_NAME = "CNN_Timeseries_Forecaster"
 
     def __init__(
-            self,
-            encode_len:int,
-            decode_len:int,
-            feat_dim:int,
-            activation:str,
-            **kwargs
-        ):
+        self, encode_len: int, decode_len: int, feat_dim: int, activation: str, **kwargs
+    ):
         """Construct a new CNN Forecaster."""
         self.encode_len = encode_len
         self.decode_len = decode_len
@@ -164,21 +173,21 @@ class Forecaster:
         self.batch_size = 64
 
         self.net = Net(
-            feat_dim = self.feat_dim,
-            decode_len = self.decode_len,
-            activation = self.activation,
-        ) 
+            feat_dim=self.feat_dim,
+            decode_len=self.decode_len,
+            activation=self.activation,
+        )
 
         self.net.to(device)
         # print(self.net.get_num_parameters()) ; sys.exit()
         self.criterion = MSELoss()
-        self.optimizer = optim.Adam( self.net.parameters() )
+        self.optimizer = optim.Adam(self.net.parameters())
         self.print_period = 1
 
-    def _get_X_and_y(self, data: np.ndarray, is_train:bool=True) -> np.ndarray:
-        """Extract X (historical target series), y (forecast window target) 
-            When is_train is True, data contains both history and forecast windows.
-            When False, only history is contained.
+    def _get_X_and_y(self, data: np.ndarray, is_train: bool = True) -> np.ndarray:
+        """Extract X (historical target series), y (forecast window target)
+        When is_train is True, data contains both history and forecast windows.
+        When False, only history is contained.
         """
         N, T, D = data.shape
         if D != self.feat_dim:
@@ -192,8 +201,8 @@ class Forecaster:
                     f"Training data expected to have {self.encode_len + self.decode_len}"
                     f" length on axis 1. Found length {T}"
                 )
-            X = data[:, :self.encode_len, :]
-            y = data[:, self.encode_len:, 0]
+            X = data[:, : self.encode_len, :]
+            y = data[:, self.encode_len :, 0]
         else:
             # for inference
             if T < self.encode_len:
@@ -201,23 +210,25 @@ class Forecaster:
                     f"Inference data length expected to be >= {self.encode_len}"
                     f" on axis 1. Found length {T}"
                 )
-            X = data[:, -self.encode_len:, :]
+            X = data[:, -self.encode_len :, :]
             y = None
         return X, y
-    
+
     def _train_on_data(
-            self, train_data: np.ndarray, valid_data: np.ndarray=None,
-            max_epochs:int=250, verbose:int=1
-        ):
+        self,
+        train_data: np.ndarray,
+        valid_data: np.ndarray = None,
+        max_epochs: int = 250,
+        verbose: int = 1,
+    ):
         """Run training on data and return the loss history"""
         train_X, train_y = self._get_X_and_y(train_data, is_train=True)
         if valid_data is not None:
-            valid_X, valid_y = self._get_X_and_y(
-                valid_data, is_train=True)
+            valid_X, valid_y = self._get_X_and_y(valid_data, is_train=True)
         else:
             valid_X, valid_y = None, None
 
-        # self.batch_size = min(train_X.shape[0] // 8, 256)
+        # self.batch_size = max(1, min(train_X.shape[0] // 8, 256))
         print(f"batch_size = {self.batch_size}")
 
         patience = get_patience_factor(train_X.shape[0])
@@ -226,21 +237,28 @@ class Forecaster:
         train_X, train_y = torch.FloatTensor(train_X), torch.FloatTensor(train_y)
         train_dataset = CustomDataset(train_X, train_y)
         train_loader = DataLoader(
-            dataset=train_dataset, batch_size=int(self.batch_size), shuffle=True)
-        
+            dataset=train_dataset, batch_size=int(self.batch_size), shuffle=True
+        )
+
         if valid_X is not None and valid_y is not None:
             valid_X, valid_y = torch.FloatTensor(valid_X), torch.FloatTensor(valid_y)
             valid_dataset = CustomDataset(valid_X, valid_y)
             valid_loader = DataLoader(
-                dataset=valid_dataset, batch_size=int(self.batch_size),  shuffle=True)
+                dataset=valid_dataset, batch_size=int(self.batch_size), shuffle=True
+            )
         else:
             valid_loader = None
 
-        losses = self._run_training(train_loader, valid_loader, max_epochs,
-                           use_early_stopping=True, patience=patience,
-                           verbose=verbose)
+        losses = self._run_training(
+            train_loader,
+            valid_loader,
+            max_epochs,
+            use_early_stopping=True,
+            patience=patience,
+            verbose=verbose,
+        )
         return losses
-    
+
     def fit(self, train_data, valid_data, pre_training_data, max_epochs=250, verbose=1):
         if pre_training_data is not None:
             print("Conducting pretraining...")
@@ -248,28 +266,35 @@ class Forecaster:
                 train_data=pre_training_data,
                 valid_data=None,
                 verbose=verbose,
-                max_epochs=max_epochs
+                max_epochs=max_epochs,
             )
         print("Training on main data...")
         history = self._train_on_data(
             train_data=train_data,
             valid_data=valid_data,
             verbose=verbose,
-            max_epochs=max_epochs
+            max_epochs=max_epochs,
         )
         self._is_trained = True
         return history
-    
-    def _run_training(self, train_loader, valid_loader, max_epochs,
-                      use_early_stopping=True, patience=10, verbose=1):
-        
+
+    def _run_training(
+        self,
+        train_loader,
+        valid_loader,
+        max_epochs,
+        use_early_stopping=True,
+        patience=10,
+        verbose=1,
+    ):
+
         best_loss = 1e7
         losses = []
         min_epochs = 10
         for epoch in range(max_epochs):
             self.net.train()
             for data in train_loader:
-                X,  y = data[0].to(device), data[1].to(device)
+                X, y = data[0].to(device), data[1].to(device)
                 # Feed Forward
                 preds = self.net(X)
                 # Loss Calculation
@@ -280,13 +305,15 @@ class Forecaster:
                 loss.backward()
                 # Weight Update: w <-- w - lr * gradient
                 self.optimizer.step()
-                
-            current_loss = loss.item()            
-            
+
+            current_loss = loss.item()
+
             if use_early_stopping:
                 # Early stopping
                 if valid_loader is not None:
-                    current_loss = get_loss(self.net, device, valid_loader, self.criterion)
+                    current_loss = get_loss(
+                        self.net, device, valid_loader, self.criterion
+                    )
                 losses.append({"epoch": epoch, "loss": current_loss})
                 if current_loss < best_loss:
                     trigger_times = 0
@@ -294,18 +321,20 @@ class Forecaster:
                 else:
                     trigger_times += 1
                     if trigger_times >= patience and epoch >= min_epochs:
-                        if verbose == 1: print(f'Early stopping after {epoch=}!')
+                        if verbose == 1:
+                            print(f"Early stopping after {epoch=}!")
                         return losses
-                
+
             else:
                 losses.append({"epoch": epoch, "loss": current_loss})
             # Show progress
             if verbose == 1:
-                if epoch % self.print_period == 0 or epoch == max_epochs-1:
-                    print(f'Epoch: {epoch+1}/{max_epochs}, loss: {np.round(current_loss, 5)}')
-            
-        return losses
+                if epoch % self.print_period == 0 or epoch == max_epochs - 1:
+                    print(
+                        f"Epoch: {epoch+1}/{max_epochs}, loss: {np.round(current_loss, 5)}"
+                    )
 
+        return losses
 
     def predict(self, data):
         X = self._get_X_and_y(data, is_train=False)[0]
@@ -313,21 +342,22 @@ class Forecaster:
         # Initialize dataset and dataloader with only X
         pred_dataset = CustomDataset(pred_X)
         pred_loader = DataLoader(
-            dataset=pred_dataset, batch_size=int(self.batch_size), shuffle=False)
-        
+            dataset=pred_dataset, batch_size=int(self.batch_size), shuffle=False
+        )
+
         all_preds = []
         for data in pred_loader:
             # Get X and send it to the device
             X = data.to(device)
             preds = self.net(X).detach().cpu().numpy()
-            preds = preds[:, -self.decode_len:]
+            preds = preds[:, -self.decode_len :]
             all_preds.append(preds)
-        
+
         preds = np.concatenate(all_preds, axis=0)
         preds = np.expand_dims(preds, axis=-1)
         return preds
 
-    def evaluate(self, test_data):         
+    def evaluate(self, test_data):
         """Evaluate the model and return the loss and metrics"""
         x_test, y_test = self._get_X_and_y(test_data, is_train=True)
         if self.net is not None:
@@ -339,20 +369,21 @@ class Forecaster:
 
     def save(self, model_path):
         model_params = {
-                "encode_len": self.encode_len, 
-                "decode_len": self.decode_len, 
-                "feat_dim": self.feat_dim, 
-                "activation": self.activation,
+            "encode_len": self.encode_len,
+            "decode_len": self.decode_len,
+            "feat_dim": self.feat_dim,
+            "activation": self.activation,
         }
         joblib.dump(model_params, os.path.join(model_path, MODEL_PARAMS_FNAME))
         torch.save(self.net.state_dict(), os.path.join(model_path, MODEL_WTS_FNAME))
-
 
     @classmethod
     def load(cls, model_path):
         model_params = joblib.load(os.path.join(model_path, MODEL_PARAMS_FNAME))
         classifier = cls(**model_params)
-        classifier.net.load_state_dict(torch.load( os.path.join(model_path, MODEL_WTS_FNAME)))        
+        classifier.net.load_state_dict(
+            torch.load(os.path.join(model_path, MODEL_WTS_FNAME))
+        )
         return classifier
 
     def __str__(self):
@@ -382,7 +413,7 @@ def train_predictor_model(
         series_len=history.shape[1],
         forecast_length=forecast_length,
         frequency=frequency,
-        num_exog=history.shape[2]-1
+        num_exog=history.shape[2] - 1,
     )
     forecast_model = Forecaster(
         encode_len=history.shape[1] - forecast_length,
@@ -398,9 +429,7 @@ def train_predictor_model(
     return forecast_model
 
 
-def predict_with_model(
-    model: Forecaster, test_data: np.ndarray
-) -> np.ndarray:
+def predict_with_model(model: Forecaster, test_data: np.ndarray) -> np.ndarray:
     """
     Make forecast.
 
